cb7c9fd903719a3faa3bcf3eb6091fbf913a200c
eoozturk <eo.ozturk@hotmail.com>
2021-11-03 21:54:33 +0300

    first test completed with get method

--- src/test/java/org/eoozturk/ApiTest.java
+++ src/test/java/org/eoozturk/ApiTest.java
@@ -9,5 +9,8 @@ public class ApiTest {
     @Test
     public void Test(){
 
+        when()
+                .get("https://reqres.in/api/users/2")
+                .then().statusCode(200);
     }
 }
